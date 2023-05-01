98e18a752e377a458e878bc3c7e31b7fb4bb9016
Adam <adamgjohnson92@gmail.com>
2023-05-01 16:30:58 -0700

    Initial commit with starter code.

---
+++ script.js
@@ -0,0 +1,34 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var passwordText = document.querySelector("#password");
+  var capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "`", "~", ",", ".", "<", ">", "/", "?", ";", ":", "{", "}", "[", "]", "|"];
+  var lengthPrompt = window.prompt("How many characters do you want your password to contain?", "Type a number between 8 and 128.")
+  if (!lengthPrompt) {
+    window.alert("Have a nice day.");
+  }
+    if (
+      (lengthPrompt < 8) ||
+      (lengthPrompt > 128))
+      {window.alert("Password must be between 8 and 128 characters long.");
+      return};
+  var capitalConfirm = window.confirm("Do you want to include capital letters?");
+  var lowerConfirm = window.confirm("Do you want to include lowercase letters?");
+  var numberConfirm = window.confirm("Do you want to include numbers?");
+  var specialConfirm = window.confirm("Do you want to include special characters?");
+  
+  var generatePassword = function() 
+  {Math.floor(Math.random() * capitalLetters.length);
+  }
+  
+  passwordText.value = generatePassword()
+
+
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
