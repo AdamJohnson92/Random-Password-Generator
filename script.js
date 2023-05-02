72b811813d46ffed4d74193e590b2a102bd4758a
Adam <adamgjohnson92@gmail.com>
2023-05-02 11:12:37 -0700

    Added var password = '' to generatePassword function to remove the undefined at the start of each password string. Next task is to get it to log onto the passwordText box and not just in the console.

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "`", "~", ",", ".", "<", ">", "/", "?", ";", ":", "{", "}", "[", "]", "|"];
 var pullOptions = [];
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   function generatePassword() {
+    var password = "";
     var lengthPrompt = window.prompt("How many characters do you want your password to contain?", "Type a number between 8 and 128.")
     if (!lengthPrompt){
     window.alert("Have a nice day.");
@@ -22,24 +24,24 @@ function writePassword() {
       {window.alert("Password must be between 8 and 128 characters long.");
       return};
   
-   var capitalConfirm = window.confirm("Do you want to include capital letters?")
+    var capitalConfirm = window.confirm("Do you want to include capital letters?")
       if (capitalConfirm) {pullOptions=pullOptions.concat(capitalLetters)}
     var lowerConfirm = window.confirm("Do you want to include lowercase letters?");
       if (lowerConfirm) {pullOptions=pullOptions.concat(lowerLetters)}
     var numbersConfirm = window.confirm("Do you want to include numbers?");
       if (numbersConfirm) {pullOptions=pullOptions.concat(numbers)}
-   var specialConfirm = window.confirm("Do you want to include special characters?");
+    var specialConfirm = window.confirm("Do you want to include special characters?");
       if (specialConfirm) {pullOptions=pullOptions.concat(specialCharacters)};
-  
-      for(var i = 0; i < lengthPrompt; i++) {
-      var index = Math.floor(Math.random() * pullOptions.length)
-      password = password + pullOptions[index]
-    }
-    console.log(password); console.log(index);
-  }
-  passwordText.value = password
+      
+      
+      for(let i = 0; i < lengthPrompt; i++) {
+      var index = Math.floor(Math.random() * pullOptions.length);
+      password = password + pullOptions[index];
+      }
+  console.log(password);
+  passwordText.value = password;
 }
-
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
