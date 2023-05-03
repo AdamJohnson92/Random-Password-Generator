869ec4eb708493d24c5948f418d3e96283c48d48
Adam <adamgjohnson92@gmail.com>
2023-05-03 16:48:54 -0700

    Fixed an issue with nested functions. All functions are now defined separately and then simply called. Broke generatePassword function down to also contain the userPrompts password which determines what characters are included in the pullOptions array.

--- script.js
+++ script.js
@@ -4,46 +4,55 @@ var capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
 var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "`", "~", ",", ".", "<", ">", "/", "?", ";", ":", "{", "}", "[", "]", "|"];
-
+var lengthPrompt;
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  
-  function generatePassword() {
-    var pullOptions = []
-    var password = "";
-    var lengthPrompt = window.prompt("How many characters do you want your password to contain?", "Type a number between 8 and 128.")
-    if (!lengthPrompt){
-    window.alert("Have a nice day.");
-    }
-    if (
-      (lengthPrompt < 8) ||
-      (lengthPrompt > 128))
-      {window.alert("Password must be between 8 and 128 characters long.");
-      return};
-  
-    var capitalConfirm = window.confirm("Do you want to include capital letters?");
-      if (capitalConfirm) {pullOptions=pullOptions.concat(capitalLetters)};
-    var lowerConfirm = window.confirm("Do you want to include lowercase letters?");
-      if (lowerConfirm) {pullOptions=pullOptions.concat(lowerLetters)};
-    var numbersConfirm = window.confirm("Do you want to include numbers?");
-      if (numbersConfirm) {pullOptions=pullOptions.concat(numbers)};
-    var specialConfirm = window.confirm("Do you want to include special characters?");
-      if (specialConfirm) {pullOptions=pullOptions.concat(specialCharacters)};
-      
-      
-      for(let i = 0; i < lengthPrompt; i++) {
-      var index = Math.floor(Math.random() * pullOptions.length);
-      password = password + pullOptions[index];
-      }
-  console.log(password);
-  
   var passwordText = document.querySelector("#password");
+  
   passwordText.value = password;
+  
 }
-return;
+
+function generatePassword() {
+  var password = "";
+  var  pullOptions = userPrompts();
+    
+    for(let i = 0; i < lengthPrompt; i++) {
+    var index = Math.floor(Math.random() * pullOptions.length);
+    password = password + pullOptions[index];
+    }
+  console.log(password);
+  return password;
+
+}
+
+function userPrompts() {
+var pullOptions = []
+lengthPrompt = window.prompt("How many characters do you want your password to contain?", "Type a number between 8 and 128.")
+if (!lengthPrompt){
+window.alert("Have a nice day.");
+}
+if (
+  (lengthPrompt < 8) ||
+  (lengthPrompt > 128))
+  {window.alert("Password must be between 8 and 128 characters long.");
+  return};
+
+  var capitalConfirm = window.confirm("Do you want to include capital letters?");
+    if (capitalConfirm) {pullOptions=pullOptions.concat(capitalLetters)};
+  var lowerConfirm = window.confirm("Do you want to include lowercase letters?");
+    if (lowerConfirm) {pullOptions=pullOptions.concat(lowerLetters)};
+  var numbersConfirm = window.confirm("Do you want to include numbers?");
+    if (numbersConfirm) {pullOptions=pullOptions.concat(numbers)};
+  var specialConfirm = window.confirm("Do you want to include special characters?");
+    if (specialConfirm) {pullOptions=pullOptions.concat(specialCharacters)};
+
+  return pullOptions;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
+
+
